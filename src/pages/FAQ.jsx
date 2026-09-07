62c16ebee29ab67cc7181e1956b021e0631421a2
dannyteejay <danieljohn3600@gmail.com>
2026-09-07 18:18:32 +0100

    match NewsletterBox imports to filename

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useMemo } from "react";
 import Title from "../components/Title";
-import NewsletterBox from "../components/Newsletter";
+import NewsletterBox from "../components/NewsletterBox";
 import { Link } from "react-router-dom";
 
 const FAQ_DATA = [
