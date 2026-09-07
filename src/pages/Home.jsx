774e491c9f48fdfc7a19bc381947dbc6d8ed6d2e
dannyteejay <danieljohn3600@gmail.com>
2026-09-07 19:14:46 +0100

    fix newsletter component import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import LatestCollection from '../components/LatestCollection'
-import BestSeller from '../components/BestSeller'
-import OurPolicy from '../components/OurPolicy'
-import NewsletterBox from '../components/NewsletterBox'
+import React from "react";
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
+import BestSeller from "../components/BestSeller";
+import OurPolicy from "../components/OurPolicy";
+import NewsletterBox from "../components/NewsletterBox";
 
 const Home = () => {
   return (
@@ -12,9 +12,9 @@ const Home = () => {
       <LatestCollection />
       <BestSeller />
       <OurPolicy />
-      <NewsLetterBox />
+      <NewsletterBox />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
